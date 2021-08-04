e5e2f979a8d17a0b6c80197d4fd527e8c0d36978
ddxxdd-code <65443542+ddxxdd-code@users.noreply.github.com>
2021-08-04 14:59:40 -0400

    Create null.asm

---
+++ src/null.asm
@@ -0,0 +1,18 @@
+# CSC258 2021Summer
+# University of Toronto
+# Final project
+# By Dedong Xie
+
+# Project started: 2021/08/02
+# Milestone1: 2021/08/03
+# Milestone2: 2021/08/04
+# Uploaded to Github: 2021/08/04
+
+# MIPS Assembly
+# Using MARS simulator
+
+# This is a useless file, for log only
+main:
+    li    $a0, 42
+    li    $v0, 1
+    syscall
